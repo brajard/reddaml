f567f8241cf1c6608a7d061614aff9bdb9c00603
brajard <julien.brajard@upmc.fr>
2020-08-22 14:40:51 +0200

    DA seems to work (check)

---
+++ L2S/compute_trainingset.py
@@ -0,0 +1,155 @@
+"""
+Python script to compute a training set for a machine learning method. If the training is computed from observation, a DA algorithm is applied.
+"""
+
+
+# Load the python modules.
+import os, sys
+import argparse
+import numpy as np
+from tqdm import tqdm
+from l2s_utils import default_param, Observator
+from sklearn.model_selection import ParameterGrid
+from dapper import EnKS, EnKF_N, with_recursion, print_averages, Chronology, GaussRV
+
+# Insert the common folder in the path (to be able to load python module)
+sys.path.insert(0,os.path.join(os.path.pardir,'common'))
+
+from toolbox import load_config, path
+
+# Parse the commande line
+parser = argparse.ArgumentParser()
+parser.add_argument('--paths',nargs='?',
+	default='config/paths.yml',
+	help='Configuration file containing the data directory')
+parser.add_argument('--params',nargs='?',
+	default='config/ref.yml',
+	help='Configuration file containing the template names of the files to load/save')
+parser.add_argument('--model',nargs='?',
+	default='config/model_trunc.yml',
+	help='Setup of the model')
+args = parser.parse_args()
+
+# Load the config files
+paths = load_config(args.paths)
+params = load_config(args.params)
+model = load_config(args.model)
+template = params['template']
+
+#Full path of files
+savedir = path(os.path.join(paths['rootdir'],params['savedir']))
+
+#File of the true simulations (used to produce observations)
+file_truth = os.path.join(savedir,template['truth'])
+
+#File of the trainingset
+file_train = os.path.join(savedir,template['train'])
+
+#Parameters to be set for a simulation
+if params['obstype'] == 'perfect':
+	used_paramter = {'N','T','seed','dtObs'}
+	print('--> use perfect observations (no DA)')
+else:
+	used_paramter = { 'p', 'std_o', 'dtObs', 'std_m' ,'N','T','seed'}
+	damethod = params['damethod']
+	paramda = params['paramda']
+	# Output of the DA
+	file_da = os.path.join(savedir, template['da'])
+	print('--> use noisy/sparse observations')
+
+#List of values for the used paramters (should in a list)
+lparam = {k:params.get(k,[default_param[k]]) for k in used_paramter}
+
+#Sequence of all the combination of the parameters
+seq_param = ParameterGrid(lparam)
+
+#Load the model
+dt = model['dt']
+model_module = __import__(model['model_module'])
+
+if model['type'] == 'physical':
+
+	HMM_trunc = getattr(model_module, model['model_name'])
+	trunc_model = with_recursion(HMM_trunc.Dyn.model)
+
+
+else:
+	raise NotImplementedError("Only physical is available as model type")
+
+for dparam in seq_param:
+	#Load True simulation
+	data_truth = np.load(file_truth.format(**dparam))
+	xx = data_truth['xx']
+	dt_full = data_truth['dt']
+	T = data_truth['T']
+	dtObs = dparam['dtObs']
+
+
+	#Modify chrono
+	HMM_trunc.t = Chronology(T=T, dt=dt, dtObs=dtObs)
+	assert HMM_trunc.t.T == T, 'chrono inconsitent, T={}, chrono.T={}'.format(T, HMM_trunc.t.T)
+	dk = int(HMM_trunc.t.dt / dt_full)
+
+	#Subsample truth
+	xxnU = xx[::dk,0,:HMM_trunc.Dyn.M]
+
+
+	if params['obstype'] == 'perfect':
+		xx_train = xxnU[HMM_trunc.t.kkObs]
+
+	else:
+		p = dparam['p']
+		std_o = dparam['std_o']
+		std_m = dparam['std_m']
+
+	# Modify observator
+		obs = Observator(t=HMM_trunc.t, p=p, std_o=std_o, m=HMM_trunc.Dyn.M, sample='random')
+		HMM_trunc.Obs = obs.h_operator()
+
+	# Modify model noise
+		sigma_m = 0.06 / np.sqrt(HMM_trunc.t.dt)  # because it is multiplied later on in DAPPER
+		HMM_trunc.Dyn.noise = GaussRV(C=sigma_m)
+
+		#Set the DA algorithm
+
+		if damethod == 'EnKF_N':
+			configda = EnKF_N(**paramda,liveplotting=False)
+		elif damethod == 'EnKS':
+			configda = EnKS(**paramda, liveplotting=False)
+		else:
+			raise NotImplementedError('DA method '+damethod+ ' is not implemented')
+
+		Obs, chrono = HMM_trunc.Obs, HMM_trunc.t
+		yy = np.zeros((chrono.KObs+1, Obs.M))
+		xxobs = np.zeros((chrono.KObs + 1, Obs.M))
+		for k, kObs, t, dt_ in tqdm(chrono.ticker, desc='Generate Obs'):
+			if kObs is not None:
+				yy[kObs] = Obs(xxnU[k], t) + Obs.noise.sample(1)
+				xxobs[kObs] = Obs(xxnU[k], t)
+		stats = configda.assimilate(HMM_trunc, xxnU, yy)
+
+		np.savez(file_da.format(**dparam), mua=stats.mu.a, muf=stats.mu.f, mus=stats.mu.s, vara=stats.var.a,
+			varf=stats.var.f, vars=stats.var.s, infl=stats.infl)
+
+		# Stats
+		avrgs = stats.average_in_time()
+		print_averages(configda, avrgs, [], ['rmse_a', 'rmv_a'])
+
+		xx_train = stats.mu.a
+
+	# Compute traininset
+
+	#Input of the dataset
+	xx_in = xx_train[:-1]
+
+	# Estimation of the true value after dtObs MTU
+	xx_out = xx_train[1:]
+
+	# Truncated value after dtObs MTU
+	xx_trunc = trunc_model(xx_in, HMM_trunc.t.dkObs, np.nan, dt)[-1]
+
+	# Estimation of the model error
+	delta = (xx_out - xx_trunc) / dtObs
+
+	#Save the training file
+	np.savez(file_train.format(**dparam), x=xx_in, y=delta, dtObs=dtObs)
--- L2S/l2s_utils.py
+++ L2S/l2s_utils.py
@@ -7,13 +7,17 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from dapper.mods.LorenzUV.lorenz95 import HMM_full, HMM_trunc as HMM_trunc_dapper
 from dapper.mods.LorenzUV.lorenz95 import LUV
 
-from dapper import with_rk4
+from dapper import with_rk4, ens_compatible, Id_mat, GaussRV, Operator
 
 # Default value of paramter (overwrited using the configuration files)
-default_param = {'p': 36, 'std_o': 1, 'dtObs': 0.05, 'dt':0.01, 'Nfil_train':1, 'N': 20, 'seed': 10, 'T':15.}
+default_param = {'p': 36, 'std_o': 1, 'dtObs': 0.05, 'dt':0.01, 'Nfil_train':1, 'N': 20, 'seed': 10, 'T':15.,
+                 'std_m':0.06}
 
 
 def plot_L96_2D(xx,xxpred,tt,labels,vmin=None,vmax=None,vdelta=None):
+	"""
+	Plot a comparison between two L96 simulations
+	"""
 	if vmin is None:
 		vmin,vmax = np.nanmin(xx),np.nanmax(xx)
 	if vdelta is None:
@@ -47,4 +51,78 @@ def other():
 # trunc HMM (no param)
 HMM_trunc = copy.deepcopy(HMM_trunc_dapper)
 HMM_trunc.Dyn.model = with_rk4(LUV.dxdt_trunc, autonom=True)
+
+import numpy as np
+from dapper import ens_compatible, Id_mat, GaussRV, Operator
+
+
+#TODO: allow list of index instead of maxind
+class Observator:
+	"""This class handles sparse observations. It allows to create a dapper-compatible observation operators
+	"""
+	def __init__(self,  t, m, max_ind=None, std_o=1, p=None, sample='random', seed_obs=2, prec=10000):
+		"""
+		Input arguments:
+		t: dapper chronoolgy of the experiment
+		m: size of the state
+		max_ind: maximum index of observations locations
+		std_o: standard deviation of observation noise
+		p: number of observation at observational time
+		sample: 'random': draw randomly p observation at each time step, 'regular': regularly sample the observation
+		seed_obs: seed for the random generator
+		prec: fixed precision on time step (t*1000 should be int) in order to have integer as dictionnary keys.
+		"""
+		self.t_ = t
+		if max_ind is None:
+			max_ind = m
+		if p is None:
+			p = max_ind
+		self.m = m
+		self.max_ind = max_ind
+		self.std_o = std_o
+		self.p = p
+		self.sample = sample
+		self.seed_obs = seed_obs
+		self.prec = prec  # Precision on time stamp (*1000 should be int)
+		self.tinds = dict()
+		self.compute_tinds()
+
+	def compute_tinds(self):
+		self.tinds = dict()
+		save_state = np.random.get_state()
+		np.random.seed(self.seed_obs)
+		for k, KObs, t_, dt in self.t.ticker:
+			if KObs is not None:
+				if self.sample == 'random':
+					self.tinds[int(self.prec*t_)] = np.sort(np.random.choice(self.max_ind, size=self.p, replace=False))
+				elif self.sample == 'regular':
+					self.tinds[int(self.prec*t_)] = np.linspace(0, self.max_ind, self.p, endpoint=False, dtype=np.int)
+		np.random.set_state(save_state)
+
+	def def_hmod(self):
+		@ens_compatible
+		def hmod(ensemble, t):
+			return ensemble[self.tinds[int(self.prec*t)]]
+		return hmod
+
+	def h_dict(self):
+		h = {'M': self.p,
+			'model': self.def_hmod(),
+			'jacob': Id_mat(self.p),
+			'noise': GaussRV(C=self.std_o * np.eye(self.p))}
+		return h
+
+	def h_operator(self):
+		h = self.h_dict()
+		return Operator(**h)
+    
+	@property
+	def t( self ):
+		return self.t_
+
+	@t.setter
+	def t( self, value ):
+		self.t_ = value
+		self.compute_tinds()
+
 
