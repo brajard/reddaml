d34ecef0267c132d5887383fb5442844a36dacd8
Julien Brajard <julien.brajard@upmc.fr>
2020-08-25 18:21:53 +0200

    Create README.ml

---
+++ L2S/README.ml
@@ -0,0 +1,13 @@
+
+
+# Run the experiments of the Lorenz 2 scale model
+
+## Configuration
+You can change the [config/paths.yml](config/paths.yml) to set the directory where to download the data
+(TODO: estimate the space needed)
+
+## Reproduce the figures of the paper
+
+## Run the model from the scripts
+
+## Run the model from the notebooks
